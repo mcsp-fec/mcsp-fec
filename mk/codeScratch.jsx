028f1886fad96b41fe9d5225f27cc4cb73e392b5
mitchellnkeo <mitchellnkeo@gmail.com>
2023-11-29 09:40:52 -0800

    Moved all my stuff to separate file.

--- mk/codeScratch.jsx
+++ mk/codeScratch.jsx
@@ -1,30 +1,45 @@
 
-//   // About to spit out hella code for localStorage sorry Quinn :T
-//   // Create a function to save to local storage.
-//   // The percentage in the below parameters would come from whatever algorithm we use to determine that.
+//   // Create a state that will manage local data
+//   // const [localData, setLocalData] = useState(0)
+//   const [correctCount, setCorrectCount] = useState(0)
+//   const [percentage, setPercentage] = useState(0)
 
-//   // percentage = numberCorrect/totalCorrect * 100
+//   const deckName = 'deckOne'
+//   const sampleSize = 25
+//   const roundedPercentage = Math.floor(percentage)
 
-//   const saveProgress = (deckId, percentage) => {
-//     // I learned that you need to set .getItem at the beginning of your code or else every time you refresh
-//     // the page, it will return an empty array.
-//     // We use JSON.parse in order to return an object that has been stringified.
-//     // All in all, the below line is to retrieve any existing progress from user's local storage.
-//     // Also assuming that userProgress is a pre-existing key-value pair within the local storage.
+//   // Function to handle setting data in the local storage 
+//   const handleSetData = () => {
 
-//     const currentProgress = JSON.parse(localStorage.getItem('userProgress')) || {}
+//     // Increase correct count
+//     setCorrectCount((prevCount) => {
+//     const newCount = prevCount + 1
+//     console.log(newCount)
+//     return newCount
+//   })
 
-//     // Once we have retrieved existing progress (or an empty key-value pair!), we can update progress with the
-//     // passed in percentage parameter and deckId that has been chosen.
+//     // Generate a unique key based on dynamic variable
+//     const newKey = deckName
 
-//     currentProgress[deckId] = percentage
+//     // Generate a new unique value based on dynamic variable
+//     const newPercentage = ((correctCount + 1) / sampleSize) * 100
 
-//     // Super pumped that we established retrieving a key-value pair and updating the pair.
-//     // The next step is to use .setItem in order to save to the local storage.
-//     // The first parameter is the name of the object we are storing the data, the second parameter
-//     // is the stringified object that will be associated within userProgress.
-
-//     localStorage.setItem('userProgress', JSON.stringify(currentProgress))
+//     // Set the item into local storage and ensure that it's stringified
+//     localStorage.setItem(newKey, JSON.stringify(newPercentage))
+//     setPercentage(newPercentage)
+//     // console.log(percentage)
 //   }
 
-//   // userProgress object --> currentProgress object which has key value pairs of deckId and percentages
+//   // Handle clearing data
+//   const handleClearData = () => {
+//     localStorage.clear();
+//     setPercentage(0); // Reset localData state after clearing
+//     setCorrectCount(0)
+//   };
+
+
+//   <div>
+//   <button onClick={handleSetData}>Correct Answer</button>
+//   <button onClick={handleClearData}>Clear Data</button>
+//   <p>{roundedPercentage}</p>
+// </div>
