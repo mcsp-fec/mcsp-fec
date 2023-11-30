ce6416510b3b937e66835b3621837101a54ff0a3
mitchellnkeo <mitchellnkeo@gmail.com>
2023-11-30 15:56:14 -0800

    Local storage added.

--- client/components/Decks.jsx
+++ client/components/Decks.jsx
@@ -1,19 +1,75 @@
 import React, { useEffect, useState } from "react";
 import styles from "./deck.module.css";
+import CorrectButtons from "./correctButtons";
 
 const Decks = () => {
   // State being used for the decks rendering on the Decks page
   const [decks, setDecks] = useState([]);
-
   const [questions, setQuestions] = useState([])
+  const [deckPercentages, setDeckPercentages] = useState({})
+
+
+  // -------------------------------- This code block will be for localStorage -----------------------------------------
+    
+  useEffect(() => {
+    // Retrieve the list of decks from our established database.
+    fetch("/api/decks")
+      .then((res) => res.json())
+      .then((decksData) => {
+        // Initialize or reinitialize local storage keys with .forEach to go through the database decks. 
+        decksData.forEach((deck) => {
+          // Set our key to the deck id, check to see if key exists by getItem and a conditional.
+          const localStorageKey = `deck${deck.id}`;
+          const existingData = localStorage.getItem(localStorageKey);
+          if (!existingData) {
+            localStorage.setItem(localStorageKey, 0);
+          }
+        });
+
+        // Set an initial values based on local storage, iterate through the fetch request, validate existingData, and update percentages accordingly
+        const initialDeckValues = {};
+        decksData.forEach((deck) => {
+          const localStorageKey = `deck${deck.id}`
+          const existingData = localStorage.getItem(localStorageKey)
+          // If data exists, parse and return it to an integer.
+          initialDeckValues[deck.id] = existingData ? parseInt(existingData) : 0
+        })
+        // Set the State of percentages with values obtained from local storage.
+        setDeckPercentages(initialDeckValues)
+      })
+      .catch((error) => console.error("You goof'd again, MITCHELL!:", error));
+  }, []);
+
+  // Function to update the State of deck percentage
+  const updateDeckPercentage = (deckId, count) => {
+    // This line will update the state of deckPercentages using the previous state
+    setDeckPercentages((prevPercentages) => ({
+      // I learned that we spread the previous state to make sure that we don't mutate it.
+      ...prevPercentages,
+      // Update the specific DeckId with its new count value. 
+      [deckId]: count,
+    }))
+  }
+
+  // Take note that we are passing our updateDeckPercentage function into our button click below - that is where our parameters of deckId and count come into play.
+
+  // Function to handle button click. This will update the local storage key associated with the deck Id 
+  const handleDeckButtonClick = (deckId) => {
+    const localStorageKey = `deck${deckId}`;
+    const currentCount = parseInt(localStorage.getItem(localStorageKey) || 0)
+    const updatedCount = currentCount + 1
+    localStorage.setItem(localStorageKey, updatedCount)
+    updateDeckPercentage(deckId, updatedCount) 
+  }
+
+  // --------------------------------- End of local storage block -------------------------------------------------------
 
   // HANDLER FOR SPECIFIC DECK ID
-  // 
   const handleDeckClick = (deckNumber) => {
     fetch(`/api/deck/${deckNumber}`)
       .then((response) => response.json())
       .then((data) => {
-      setQuestions(data)})
+      console.log(data)})
       .catch((error) => console.error("Error fetching deck:", error));
   };
 
@@ -25,6 +81,7 @@ const Decks = () => {
         onClick={() => handleDeckClick(deck.id)}
       >
         {`Deck ${deck.id}`}
+        <p>{deckPercentages[deck.id]}%</p>
         <p>{deck.description}</p>
       </div>
     ));
@@ -50,6 +107,7 @@ const Decks = () => {
   return (
     <div style={containerStyle}>
       <main>{renderDeckBoxes()}</main>
+      <CorrectButtons decks={decks} handleDeckButtonClick={handleDeckButtonClick}/>
     </div>
   );
 };
